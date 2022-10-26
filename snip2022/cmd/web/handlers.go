cb89136965e1cf316227922df76cc87529b8297b
samllX <xieyu559@163.com>
2022-10-26 18:51:12 +0800

    update home handler -- add base tmpl

--- snip2022/cmd/web/handlers.go
+++ snip2022/cmd/web/handlers.go
@@ -13,19 +13,30 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	// 使用 template.ParseFiles() 函数读模板文件。如果有错误，
-	// 我们可以通过 log 输出详细的错误信息并使用 http.Error() 函数发送
+
+	// 初始化一个包含两个文件的切片。
+	// 注意：包含 base template 的必须是切片中的第一个元素
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+
+	// 使用 template.ParseFiles() 函数读取文件并保存模板。
+	// 注意：我们能通过文件路径的切片作为一个可变参数？
+	// 如果有错误，我们可以通过 log 输出详细的错误信息
+	// 并使用 http.Error() 函数发送
 	// 通用的内部服务出错的 500 错误信息给用户。
-	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 
-	// 使用 Execute() 方法来写模板内容作为响应体。其中最后一个参数代表任意一个我们想
-	// 传入的动态数据，这里我们什么也不传，使用 nil 填充。
-	err = ts.Execute(w, nil)
+	// 使用 ExecuteTemplate() 方法来写 base 模板中的内容作为响应体。
+	// 其中最后一个参数代表任意一个我们想传入的动态数据，
+	// 这里我们什么也不传，使用 nil 填充。
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", 500)

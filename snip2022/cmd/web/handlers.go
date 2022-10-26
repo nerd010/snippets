c1d839b79b5c580556c010619de4736bf1046dac
samllX <xieyu559@163.com>
2022-10-26 18:34:11 +0800

    update home handler -- add tmpl page

--- snip2022/cmd/web/handlers.go
+++ snip2022/cmd/web/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -10,6 +12,23 @@ func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
+	}
+	// 使用 template.ParseFiles() 函数读模板文件。如果有错误，
+	// 我们可以通过 log 输出详细的错误信息并使用 http.Error() 函数发送
+	// 通用的内部服务出错的 500 错误信息给用户。
+	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	// 使用 Execute() 方法来写模板内容作为响应体。其中最后一个参数代表任意一个我们想
+	// 传入的动态数据，这里我们什么也不传，使用 nil 填充。
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
 	}
 
 	w.Write([]byte("Hello from Snippetbox"))
